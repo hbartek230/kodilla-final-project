692090696f64c81793a51157b75ec1c08f16e41c
hbartek230 <bhreniak@gmail.com>
2020-09-17 13:45:41 +0200

    Start with ending project

---
+++ src/main/java/com/kodilla/exchangesystem/ExchangesystemApplication.java
@@ -0,0 +1,13 @@
+package com.kodilla.exchangesystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExchangesystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExchangesystemApplication.class, args);
+    }
+
+}
